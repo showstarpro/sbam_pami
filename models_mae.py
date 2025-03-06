360c94785f00d0a1e943b636c7f8c434bd75a0f7
showstarpro <1359947339@qq.com>
2025-03-06 07:19:12 +0000

    add pami for sbam

--- models_mae.py
+++ models_mae.py
@@ -145,9 +145,10 @@ class MaskedAutoencoderViT(nn.Module):
         x = self.decoder_embed(x)
 
         mask_tokens = self.mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1)
-        x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)
-        x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))
-        x = torch.cat([x[:, :1, :], x_], dim=1)
+        x  = torch.cat([x, mask_tokens], dim=1)
+        # x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)
+        # x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))
+        # x = torch.cat([x[:, :1, :], x_], dim=1)
 
         x = x + self.decoder_pos_embed
 
