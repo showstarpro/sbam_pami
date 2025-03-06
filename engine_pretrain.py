7b5e3692758250fd377c956ca303430230c5a66a
showstarpro <1359947339@qq.com>
2025-03-06 07:12:35 +0000

    add linpro

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -34,7 +34,7 @@ def train_one_epoch(model: torch.nn.Module,
         samples = samples.to(device, non_blocking=True)
 
         with torch.cuda.amp.autocast():
-            loss, _, _ = model(samples)
+            loss, _, _ = model(samples, base=args.mask_ratio)
 
         loss_value = loss.item()
 
